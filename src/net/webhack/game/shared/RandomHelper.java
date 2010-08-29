31f010d88bc95a1a397aabae98ad487fa210a68c
Jeff Bailey <jeffbailey@google.com>
2010-08-28 23:33:48 -0400

    Add missing Random-related files, add first test suite.  (Things fail, but coverage analysis is working as is the test suite)

---
+++ src/net/webhack/game/shared/RandomHelper.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2010 Google Inc.  Nethack Public License.
+ */
+
+package net.webhack.game.shared;
+
+/**
+ * @author Jeff Bailey <jeffbailey@google.com>
+ * 
+ */
+public interface RandomHelper {
+
+	/** 0 <= rn2(x) < x */
+	int rn2(int x);
+
+	/**
+	 * 0 <= rnl(x) < x; sometimes subtracting Luck
+	 * 
+	 * good luck approaches 0, bad luck approaches (x-1)
+	 */
+	int rnl(int x);
+
+	/** 1 <= rnd(x) <= x */
+	int rnd(int x);
+
+	/** n <= d(n,x) <= (n*x) */
+	int d(int n, int x);
+
+	int rne(int x);
+
+	int rnz(int i);
+}
---
+++ test/net/webhack/game/shared/RectanglesTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2010 Google Inc.  Nethack Public License.
+ */
+
+package net.webhack.game.shared;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Jeff Bailey <jeffbailey@google.com>
+ *
+ */
+public class RectanglesTest {
+
+	/**
+	 * Test method for {@link net.webhack.game.shared.Rectangles#Rectangles(net.webhack.game.shared.RandomHelper)}.
+	 */
+	@Test
+	public void testRectangles() {
+		new Rectangles(null);
+	}
+
+	/**
+	 * Test method for {@link net.webhack.game.shared.Rectangles#getRectInd(net.webhack.game.shared.Rectangle)}.
+	 */
+	@Test
+	public void testGetRectInd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.webhack.game.shared.Rectangles#getRect(net.webhack.game.shared.Rectangle)}.
+	 */
+	@Test
+	public void testGetRect() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.webhack.game.shared.Rectangles#removeRect(net.webhack.game.shared.Rectangle)}.
+	 */
+	@Test
+	public void testRemoveRect() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link net.webhack.game.shared.Rectangles#addRect(net.webhack.game.shared.Rectangle)}.
+	 */
+	@Test
+	public void testAddRect() {
+		fail("Not yet implemented");
+	}
+
+}
