0200a67cdbfa12776a6b4f5d6b18e2b3f0807d52
Jeff Bailey <jeffbailey@google.com>
2010-10-17 00:24:19 -0400

    Add Okay button, have box disappear on clicking it rather than outside the box.

--- src/net/webhack/game/client/RoleRaceGender.java
+++ src/net/webhack/game/client/RoleRaceGender.java
@@ -4,18 +4,48 @@
 
 package net.webhack.game.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * @author Jeff Bailey <jeffbailey@google.com>
  * 
  */
-public class RoleRaceGender extends PopupPanel {
+public class RoleRaceGender extends PopupPanel implements ClickHandler {
+
+	final ListBox roleSelector;
+
 	public RoleRaceGender() {
-		super(true);
+		super(false);
 		setGlassEnabled(true);
 
-		setWidget(new Label("Choose Race"));
+		final VerticalPanel panel = new VerticalPanel();
+		panel.add(new Label("Choose Role"));
+
+		roleSelector = new ListBox();
+		roleSelector.addItem("Archeologist");
+		roleSelector.addItem("Wizard");
+
+		roleSelector.setVisibleItemCount(1);
+
+		panel.add(roleSelector);
+
+		final Button okButton = new Button("Start", this);
+		panel.add(okButton);
+
+		setWidget(panel);
+
+	}
+
+	public void onClick(final ClickEvent event) {
+		// System.out.println(roleSelector.getValue(roleSelector
+		// .getSelectedIndex()));
+
+		this.hide();
 	}
 }
