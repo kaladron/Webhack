7f788c306877fc61f90ecc99f1156b2c9834369a
Jeff Bailey <jeffbailey@google.com>
2010-12-12 22:47:04 -0500

    Add @Documented to @Stub

--- src/net/webhack/game/shared/Stub.java
+++ src/net/webhack/game/shared/Stub.java
@@ -4,10 +4,15 @@
 
 package net.webhack.game.shared;
 
+import java.lang.annotation.Documented;
+
 /**
+ * Indicates that the annotated method is not to be considered complete.
+ * 
  * @author Jeff Bailey <jeffbailey@google.com>
- *
+ * 
  */
+@Documented
 public @interface Stub {
 
 }
