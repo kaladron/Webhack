fb347568d7d7fc0176a51a2a622ea090b02701a4
Jeff Bailey <jeffbailey@google.com>
2011-03-02 22:59:07 -0800

    Implement vision in terms of vision array.
    Add Class for implementing Algorithm C, but retool VisionSimple to
    work with the new array so we've got something in the meantime.

--- src/net/webhack/game/shared/Vision.java
+++ src/net/webhack/game/shared/Vision.java
@@ -4,10 +4,39 @@
 
 package net.webhack.game.shared;
 
-public interface Vision {
-	boolean cansee(final int x, final int y);
+public abstract class Vision {
 
-	void setBindery(Bindery bindery);
+	protected Bindery bindery;
 
-	void vision_recalc(int control);
+	protected Boolean[][] viz_array = new Boolean[Webhack.ROWNO][Webhack.COLNO];
+
+	public Vision() {
+		for (int y = 0; y < Webhack.ROWNO; y++) {
+			for (int x = 0; x < Webhack.COLNO; x++) {
+				viz_array[y][x] = new Boolean(false);
+			}
+		}
+	}
+
+	public boolean cansee(final int x, final int y) {
+		return viz_array[y][x];
+	}
+
+	public void setBindery(final Bindery bindery) {
+		this.bindery = bindery;
+	}
+
+	public void vision_recalc(final int control) {
+		view_from(bindery.you.uy, bindery.you.ux);
+
+		for (int y = 0; y < Webhack.ROWNO; ++y) {
+			for (int x = 0; x < Webhack.COLNO; ++x) {
+				if (viz_array[y][x]) {
+					bindery.ui.newsym(x, y);
+				}
+			}
+		}
+	}
+
+	abstract void view_from(int y, int x);
 }
---
+++ src/net/webhack/game/shared/VisionAlgorithmC.java
@@ -0,0 +1,11 @@
+package net.webhack.game.shared;
+
+public class VisionAlgorithmC extends Vision {
+
+	@Override
+	void view_from(final int y, final int x) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
